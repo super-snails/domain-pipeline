b6364daf08e48a2b781eda17845343526e22886a
tsing <ld320321@163.com>
2021-08-12 23:16:05 +0800

    低层服务第一版本

---
+++ src/main/java/com/tsing/product/api/demo/service/DemoService.java
@@ -0,0 +1,15 @@
+package com.tsing.product.api.demo.service;
+
+/**
+ * <p>
+ * 服务状态检查 服务类
+ * </p>
+ *
+ * @author dong.li
+ * @since 2021-07-31
+ */
+public interface DemoService {
+
+    String checkHealth();
+
+}
